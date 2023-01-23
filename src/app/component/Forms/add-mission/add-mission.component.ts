ff3c1c8e3b050927da220b66b43a02ff6212a515
ManarYousry <77962166+ManarYousry@users.noreply.github.com>
2023-01-23 21:05:36 +0200

    fix attatch

--- src/app/component/Forms/add-mission/add-mission.component.ts
+++ src/app/component/Forms/add-mission/add-mission.component.ts
@@ -1,7 +1,6 @@
-
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { MatAccordion } from '@angular/material/expansion';
+
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { MissionList } from 'src/app/model/mission-list';
@@ -14,95 +13,94 @@ import { MissionService } from 'src/app/shared/service/mission.service';
 @Component({
   selector: 'app-add-mission',
   templateUrl: './add-mission.component.html',
-  styleUrls: ['./add-mission.component.css']
+  styleUrls: ['./add-mission.component.css'],
 })
 export class AddMissionComponent implements OnInit {
-  submittedfile=false;
-  appear=false;
-  sameTeam=true;
+  submittedfile = false;
+  appear = false;
+  sameTeam = true;
   file_store: FileList;
   file_list: Array<string> = [];
-  file: File = null; // Variable to store file
+  file: File |null; // Variable to store file
   fileName: string;
 
   missionTypeList: MissionType[] = [];
-missionList:MissionList[]=[];
-  constructor(public dialogRef: MatDialogRef<AddMissionComponent>,
-    public service: MissionFormService, public missionService: MissionService,
-    private toastr: ToastrService,private _router:Router) {
-  }
+  missionList: MissionList[] = [];
+  constructor(
+    public dialogRef: MatDialogRef<AddMissionComponent>,
+    public service: MissionFormService,
+    public missionService: MissionService,
+    private toastr: ToastrService,
+    private _router: Router
+  ) {}
 
   ngOnInit() {
     this.service.initializeFormGroup();
-    this.missionService.getLists().subscribe(res => {
+    this.missionService.getLists().subscribe((res) => {
       if (res.status == true) {
         this.missionTypeList = res.missionTypesList;
-      }
-      else {
+      } else {
         this.toastr.warning('Failed');
       }
-    })//end of subscribe
+    }); //end of subscribe
   }
 
-   // On file Select
+  // On file Select
 
   onSubmit() {
-    debugger
-    if(!this.service.form.valid) {
+    debugger;
+    if (!this.service.form.valid) {
       return;
-    }//end of if
-    let missionn={
-      id:this.service.form.value.id,
-     // jobDegree:this.service.form.value.jobDegree,
-      jobDegree:this.service.form.value.jobDegreeName,
+    } //end of if
+    let missionn = {
+      id: this.service.form.value.id,
+      // jobDegree:this.service.form.value.jobDegree,
+      jobDegree: this.service.form.value.jobDegreeName,
       missionPurpose: this.service.form.value.missionPurpose,
-      centerOfCost:this.service.form.value.centerOfCost,
-      companyType:this.service.form.value.companyType,
+      centerOfCost: this.service.form.value.centerOfCost,
+      companyType: this.service.form.value.companyType,
       missionPlace: this.service.form.value.missionPlace,
       startDateMission: this.service.form.value.startDateMission,
-      endDateMission:this.service.form.value.endDateMission,
-      startDateStay:this.service.form.value.startDateStay,
+      endDateMission: this.service.form.value.endDateMission,
+      startDateStay: this.service.form.value.startDateStay,
       endDateStay: this.service.form.value.endDateStay,
       noOfNights: this.service.form.value.noOfNights,
-      stay:this.service.form.value.stay,
-      mealsAndIncidentals:this.service.form.value.mealsAndIncidentals,
+      stay: this.service.form.value.stay,
+      mealsAndIncidentals: this.service.form.value.mealsAndIncidentals,
       jobNumber: this.service.form.value.jobNumber,
       missionTypeCost: this.service.form.value.missionTypeCost,
-      permissionRequest:this.service.form.value.permissionRequest,
+      permissionRequest: this.service.form.value.permissionRequest,
       permissionDuration: this.service.form.value.permissionDuration,
-      comment:this.service.form.value.comment,
+      comment: this.service.form.value.comment,
       //attachFile: this.file,
-      file:'' ,
-      creationDate:this.service.form.value.creationDate,
-      createdBy:this.service.form.value.createdBy,
-      updateDate:this.service.form.value.updateDate,
-      updateBy:this.service.form.value.updateBy,
-      statusId:this.service.form.value.statusId,
-      missionTypeId:this.service.form.value.missionTypeId,
-      userId:this.service.form.value.userId
-    }//end of mission
+      file: '',
+      creationDate: this.service.form.value.creationDate,
+      createdBy: this.service.form.value.createdBy,
+      updateDate: this.service.form.value.updateDate,
+      updateBy: this.service.form.value.updateBy,
+      statusId: this.service.form.value.statusId,
+      missionTypeId: this.service.form.value.missionTypeId,
+      userId: this.service.form.value.userId,
+    }; //end of mission
 
-
-      this.missionService.addMission(missionn).subscribe(res=>
-        {
-          if(res.status==true)
-          {
-            if(this.file!=null){
-             this.missionService.upload(this.file,res.id).subscribe(res=>{console.log(res.status)})
-           }
-            this.toastr.success(":added successfully");
-            this.service.form.reset();
-            this.dialogRef.close('save');
-          }
-          else
-          {
-            this.toastr.warning(":failed");
-          }
-        })
+    this.missionService.addMission(missionn).subscribe((res) => {
+      if (res.status == true) {
+        if (this.file != null) {
+          this.missionService.upload(this.file, res.id).subscribe((res) => {
+            console.log(res.status);
+          });
+        }
+        this.toastr.success(':added successfully');
+        this.service.form.reset();
+        this.dialogRef.close('save');
+      } else {
+        this.toastr.warning(':failed');
+      }
+    });
     this.onClose();
     this.dialogRef.close('save');
     //this._router.navigate(['/summary'] );
-  }//end of submit
+  } //end of submit
   onClear() {
     this.service.form.reset();
     this.service.initializeFormGroup();
@@ -113,48 +111,40 @@ missionList:MissionList[]=[];
     this.service.initializeFormGroup();
     this.dialogRef.close('save');
   }
-search(jobNumber:any){
-  this.missionService.checkSameTeam(jobNumber).subscribe(res=>{
-if(res.status)
-{
-  this.service.form['controls']['jobNumber'].setValue(res.jobNumber)
-  this.service.form['controls']['userName'].setValue(res.name)
-  this.service.form['controls']['userId'].setValue(res.userId)
-   this.service.form['controls']['jobDegreeName'].setValue(res.jobDegreeName)
-  this.appear =!this.appear
-  this.sameTeam=true;
-}
-else
-{
-  this.sameTeam=false;
-}
-
-  })
-}
-
-
-handleFileInputChange(event){
-  this.file = event.target.files[0];
- this.fileName = event.target.files[0].name;
- this.service.form.controls['attachFile'].setValue(this.fileName)
- this.submittedfile= true;
-
-}
+  search(jobNumber: any) {
+    this.missionService.checkSameTeam(jobNumber).subscribe((res) => {
+      if (res.status) {
+        this.service.form['controls']['jobNumber'].setValue(res.jobNumber);
+        this.service.form['controls']['userName'].setValue(res.name);
+        this.service.form['controls']['userId'].setValue(res.userId);
+        this.service.form['controls']['jobDegreeName'].setValue(
+          res.jobDegreeName
+        );
+        this.appear = !this.appear;
+        this.sameTeam = true;
+      } else {
+        this.sameTeam = false;
+      }
+    });
+  }
 
 
 
-onChange(event) {
-  this.file = event.target.files[0];
-}
+  // onChange(event) {
+  //   this.file = event.target.files[0];
+  // }
+  handleFileInputChange(event) {
+    this.file = event.target.files[0];
+    this.fileName = event.target.files[0].name;
+    this.service.form['controls']['attachFile'].setValue(this.fileName);
 
+  }
+  removeFile() {
+    this.file = null;
+    this.fileName ='';
+   this.service.form['controls']['attachFile'].setValue('');
+  
 
-
-removeFile() {
-  this.file = null;
-  this.fileName = '';
-  this.service.form.controls['attachFile'].setValue('')
-  this.submittedfile=false;
-  //this.file_list.splice(i,1);
-}
+  }
 
 }
--- src/app/component/Forms/edit/edit.component.ts
+++ src/app/component/Forms/edit/edit.component.ts
@@ -108,7 +108,7 @@ export class EditComponent implements OnInit {
   this.attachName=this.data.attachFilename;
   console.log(this.attachName);
   }
- 
+
 
   onSubmit() {
     if(!this.service.form.valid){
@@ -182,18 +182,20 @@ export class EditComponent implements OnInit {
   //edit on attach file
   handleFileInputChange(event){
     this.file = event.target.files[0];
-   this.fileName = event.target.files[0].name;
-   this.submittedfile=true;
-  
+    this.attachName = event.target.files[0].name;
+   this.service.form['controls']['attachFile'].setValue(this.fileName);
+
   }
 
 
- 
+
   removeFile(id:number) {
-    this.missionService.DeleteAttachFile(id).subscribe(res=>{console.log("hh")});
-    this.file = null;
-    this.fileName = '';
-    this.submittedfile=false;
+    this.missionService.DeleteAttachFile(id).subscribe(res=>{
+      this.file = null;
+      this.attachName = '';
+      this.service.form['controls']['attachFile'].setValue('');
+
+      console.log("hh")});
 
   }
 }
