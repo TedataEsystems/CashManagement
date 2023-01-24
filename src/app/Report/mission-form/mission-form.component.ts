8090070ee93d21313fea02b98398647e8f5908a8
Abdelrahman Ali <[abdo.ali1012@outlook.com]>
2023-01-24 13:27:18 +0200

    update Mission Form

--- src/app/Report/mission-form/mission-form.component.ts
+++ src/app/Report/mission-form/mission-form.component.ts
@@ -1,54 +1,43 @@
-import { Component,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
 import { MissionService } from 'src/app/shared/service/mission.service';
-
 @Component({
   selector: 'app-mission-form',
   templateUrl: './mission-form.component.html',
   styleUrls: ['./mission-form.component.css']
 })
 export class MissionFormComponent implements OnInit {
-
-  constructor(private missionService:MissionService) { }
-mision:any;
-misionDuration:any;
-x:any;
+  constructor(private missionService: MissionService) { }
+  mision: any;
+  misionDurationEn: any;
+  misionDurationAr: any;
   ngOnInit(): void {
-
-    this.mision=this.missionService.missionForm;
-    this.x=this.missionService.missionForm.startDateMission.getDate;
-    console.log(this.x,"xxx")
-    console.log(this.missionService.missionForm.startDateMission);
-    console.log(this.missionService.missionForm.endDateMission);
-    //this.misionDuration=this.missionService.missionForm.endDateMission-this.missionService.missionForm.startDateMission;
-    console.log(this.misionDuration,"mission");
-    
-
+    this.mision = this.missionService.missionForm;
+    let startDateMission = new Date(this.missionService.missionForm.startDateMission);
+    let endDateMission = new Date(this.missionService.missionForm.endDateMission)
+    var time = (endDateMission.getTime() - startDateMission.getTime()) / (1000 * 3600 * 24);
+    this.misionDurationEn = " " + time + "  Days ";
+    this.misionDurationAr = time + " يوم"
   }
-
-
   public exportPDF() {
-
-    const div = document.getElementById('pdfTable');console.log(div)
+    const div = document.getElementById('pdfTable'); console.log(div)
     const options = {
       background: 'white',
       scale: 2,
     };
-
-    html2canvas(div as HTMLElement,options).then((canvas) => {
+    html2canvas(div as HTMLElement, options).then((canvas) => {
       debugger;
       console.log("ed");
       var img = canvas.toDataURL("image/PNG");
       var doc = new jsPDF('l', 'mm', 'a4');
-
       // Add image Canvas to PDF
       const bufferX = 5;
       const bufferY = 5;
       const imgProps = (<any>doc).getImageProperties(img);
       const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      console.log(pdfHeight + " / "+pdfWidth)
+      console.log(pdfHeight + " / " + pdfWidth)
       doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, 220, undefined, 'FAST');
 
       return doc;
@@ -57,9 +46,9 @@ x:any;
       doc.save(`Mission-Form${Date.now()}.pdf`);
 
 
-    //  this.toastr.success("pdf downloaded");
+      //  this.toastr.success("pdf downloaded");
 
     });
 
-}
+  }
 }
